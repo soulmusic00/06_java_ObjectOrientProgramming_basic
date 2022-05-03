b7302b670bc2ee4661b4535bd8aadb78e8be28dc
12_web_isaki <12_web_isaki@DESKTOP-2521CRI>
2022-05-03 17:45:42 +0900

    06_java_ObjectOrientProgramming_basic

---
+++ 06_java_ObjectOrientProgramming_basic/src/step6_02/method/MethodEx04.java
@@ -0,0 +1,48 @@
+package step6_02.method;
+
+/*
+ * 
+ * # this : 객체(인스턴스) 자신
+ * 
+ * */
+class Zealot {
+	
+	int shield = 60;
+	int hp = 100;
+	
+	void showStatus() {
+		System.out.println("this : " + this);
+		System.out.println("shield : " + this.shield);
+		System.out.println("hp : " + this.hp);
+		System.out.println();
+		
+		// this는 생략 가능하다.
+		//System.out.println("shield : " + shield);
+		//System.out.println("hp : " + hp);
+		
+	}
+	
+}
+
+public class MethodEx04 {
+
+	public static void main(String[] args) {
+
+		Zealot z1 = new Zealot();
+		System.out.println(z1);
+		z1.shield = 0;
+		z1.showStatus();
+		
+		Zealot z2 = new Zealot();
+		System.out.println(z2);
+		z2.shield = 0;
+		z2.hp = 0;
+		z2.showStatus();
+		
+		Zealot z3 = new Zealot();
+		System.out.println(z3);
+		z3.showStatus();
+		
+	}
+
+}
